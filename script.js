3632dd82b392bed79b0ea2789ab548bddee2a7ed
Manish Thakur <manisthakur2004@gmail.com>
2025-04-22 22:30:01 +0545

    code for hide and unhide elements

---
+++ script.js
@@ -0,0 +1,15 @@
+const myImg = document.getElementById('myImg');
+const myButton = document.getElementById('mybutton');
+
+myButton.addEventListener('click', () => {
+
+    if (myImg.style.visibility === "hidden") {
+        myImg.style.visibility = "visible";
+        myButton.textContent = "hide";
+    } else {
+        myImg.style.visibility = "hidden";
+        myButton.textContent = "show";
+    }
+
+
+})
